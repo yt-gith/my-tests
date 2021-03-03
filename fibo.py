394db786341535852242d9c151854a3f5f4c4c55
yt <trostin@gmail.com>
2021-03-03 21:51:08 +0300

    convert double quotes to single

--- fibo.py
+++ fibo.py
@@ -1,11 +1,11 @@
 import sys
 
 # printnew_func()def new_func():
-name = "test"
-ss = f"var name=\"{name}\""
+name = 'test'
+ss = f'var name="{name}"'
 print(ss)
 a = True
 print(sys.__name__)
 
 # добавляем какой то код
-print("oops")
+print('oops')
